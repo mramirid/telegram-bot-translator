5ad94a5373d79ed44094366c46375d2ff0da0c8e
Amir <amir.rhythm@gmail.com>
2020-03-17 18:01:31 +0700

    Optimizing

--- index.php
+++ index.php
@@ -2,24 +2,22 @@
 
 $token = file_get_contents('TOKEN.txt');
 
-require_once "curl_helper.php";
+$updates = json_decode(file_get_contents("https://api.telegram.org/bot$token/getUpdates"), TRUE);
 
-$update = json_decode($response, TRUE);
-print_r($update); exit;
-
-$message = $update["message"];
+$message = $updates["result"][0]["message"];
+print_r($message["text"]); exit;
 
 // Buat beberapa variabel untuk identifikasi chatid dan text yang akan dikirim
 $chatId = $message["chat"]["id"];
 $text = $message["text"];
 
-//Buat Koneksi ke database
+// Buat Koneksi ke database
 // $server = "localhost"; //ganti sesuai server Anda
 // $username = "  "; //ganti sesuai username Anda
 // $password = "   "; //ganti sesuai password Anda
 // $db_name = "  "; //ganti sesuatu nama database Anda
 
-// //Buat Koneksi ke MySQLi di Hosting
+//Buat Koneksi ke MySQLi di Hosting
 // $conn=mysqli_connect($server,$username,$password,$db_name);
 
 // // Melakukan Pengecekan Koneksi
@@ -27,10 +25,10 @@ $text = $message["text"];
 //     echo "gagal masuk ke MySQLL: " . mysqli_connect_error();
 // }
 
-//mengambil isi pesan dari tabel pesan lalu mengirimkannya ke variabel $text
+// mengambil isi pesan dari tabel pesan lalu mengirimkannya ke variabel $text
 // $query = mysqli_query($conn,"select pesan from reply where keyword='".$text."'";
 
-//pengecekan bila data yang dicari tidak ada, memberikan pesan balasan
+// pengecekan bila data yang dicari tidak ada, memberikan pesan balasan
 // if (mysqli_num_rows($query)>0) {
 //     while ($hasilbalasan = mysqli_fetch_row($query)) {
 //         $balas = $hasilbalasan['0'];
