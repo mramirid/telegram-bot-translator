80be43b29683e9b1bea7e87c0b1af0f4aaabd5f1
Sunu Ilham Pradika <sunudika@gmail.com>
2020-03-19 13:55:45 +0700

    update return value for reply to users

--- core/CoinIDR.php
+++ core/CoinIDR.php
@@ -31,7 +31,9 @@ class CoinIDR
          global $coin_idr_markets;
 
          if(in_array($this->coin, $coin_idr_markets)) {
-            $responses .= "Data Koin $this->coin\n";
+            $name = strtoupper(substr($this->coin, 0, 3));
+            $responses .= "Data Koin $name\n";
+            
             $responses .= 'Harga Terakhir'. ' Rp.' . number_format($this->tickers[$this->coin]['last'], 0, ',', '.') . PHP_EOL;
             $responses .= 'Harga Tertinggi'. ' Rp.' . number_format($this->tickers[$this->coin]['high'], 0, ',', '.') . PHP_EOL;
             $responses .= 'Harga Terendah'. ' Rp.' . number_format($this->tickers[$this->coin]['low'], 0, ',', '.') . PHP_EOL;
