909c8411204c391e388a95a0aca8890a64785a69
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-04-23 23:28:36 +0700

    Tuning

--- bot.php
+++ bot.php
@@ -4,9 +4,6 @@ use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
 use BotMan\Drivers\Telegram\TelegramDriver;
-use BotMan\BotMan\Messages\Attachments\Image;
-use BotMan\BotMan\Messages\Attachments\Video;
-use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 
 require_once "vendor/autoload.php";
 require_once "database/config.php";
@@ -29,9 +26,25 @@ $botman->hears("/start", function (BotMan $bot) {
     $bot->reply("Willkommen " . $user->getFirstName() . " 😊 (id: " . $user->getId() . ")");
 });
 
+$botman->hears("/start@api_2020_bot", function (BotMan $bot) {
+    $user = $bot->getUser();
+    insertUserIfNecessary($user);   // Daftarkan user
+    $bot->reply("Willkommen " . $user->getFirstName() . " 😊 (id: " . $user->getId() . ")");
+});
+
 $botman->hears("/help", function (BotMan $bot) {
     insertUserIfNecessary($bot->getUser());
-    $message = "/say hai - Menyapa bot" . PHP_EOL . "/say kenalan - Info mengenai bot";
+    $message  = "/say@api_2020_bot hai - Menyapa bot" . PHP_EOL . PHP_EOL;
+    $message .= "/say@api_2020_bot kenalan - Info mengenai bot" . PHP_EOL . PHP_EOL;
+    $message .= "Tekan perintah selama 1-3 detik untuk memilih perintah";
+    $bot->reply($message);
+});
+
+$botman->hears("/help@api_2020_bot", function (BotMan $bot) {
+    insertUserIfNecessary($bot->getUser());
+    $message  = "/say@api_2020_bot hai - Menyapa bot" . PHP_EOL . PHP_EOL;
+    $message .= "/say@api_2020_bot kenalan - Info mengenai bot" . PHP_EOL . PHP_EOL;
+    $message .= "Tekan perintah selama 1-3 detik untuk memilih perintah";
     $bot->reply($message);
 });
 
@@ -40,11 +53,17 @@ $botman->hears("/say {message}", function (BotMan $bot, $message) {
     $bot->reply(getResponse($bot->getUser(), $message));
 });
 
+$botman->hears("/say@api_2020_bot {message}", function (BotMan $bot, $message) {
+    insertUserIfNecessary($bot->getUser());
+    $bot->reply(getResponse($bot->getUser(), $message));
+});
+
 // Fallback (balasan invalid command)
 $botman->fallback(function (BotMan $bot) {
     insertUserIfNecessary($bot->getUser());
-    $message = $bot->getMessage()->getText();
-    $bot->reply("Invalid command for '$message'");
+    $message  = "Invalid command for " . $bot->getMessage()->getText() . PHP_EOL . PHP_EOL;
+    $message .= "Mungkin anda kurang input argumen perintah? Cek /help atau /help@api_2020_bot";
+    $bot->reply($message);
 });
 
 $botman->listen();
