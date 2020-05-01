19aed068ba3bd9757a5ee7e5e9419f649eb674e5
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-05-01 18:25:01 +0700

    Update db

--- assets/demo_bot.sql
+++ assets/demo_bot.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 01, 2020 at 04:32 PM
--- Server version: 5.7.30-log
--- PHP Version: 7.3.6
+-- Generation Time: May 01, 2020 at 11:24 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `amirrpw_demo_bot`
+-- Database: `demo_bot`
 --
 
 -- --------------------------------------------------------
@@ -36,15 +35,6 @@ CREATE TABLE `count_messages` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `count_messages`
---
-
-INSERT INTO `count_messages` (`id`, `id_user`, `id_message`, `count_usage`, `created_at`, `updated_at`) VALUES
-(1, 482942544, 1, 7, '2020-04-23 16:06:47', '2020-04-29 14:46:50'),
-(2, 482942544, 2, 2, '2020-04-23 16:08:15', '2020-04-24 22:31:39'),
-(3, 753050347, 1, 2, '2020-04-23 16:23:48', '2020-04-23 16:23:59');
 
 -- --------------------------------------------------------
 
@@ -79,15 +69,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `nickname`) VALUES
-(482942544, 'mramirid', 'Amir'),
-(753050347, 'sunuilhmp', 'Sunu'),
-(788192855, 'kholil_rnm', 'Kholilul');
-
---
 -- Indexes for dumped tables
 --
 
@@ -117,7 +98,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `count_messages`
 --
 ALTER TABLE `count_messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `messages`
